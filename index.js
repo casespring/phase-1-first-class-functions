c558f1c4fbe70f798d4796b7e71fd56b1c26c191
Marco Upia <markremembers@gmail.com>
2023-10-31 21:07:23 -0400

    callback functions. I need to practise alot

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-
-
-function receivesAFunction(callback) {
-    return callback();
+function receivesAFunction(cb) {
+    return cb();
 };
 
 function returnsANamedFunction() {
-     return  function named() {
-        console.log("My return");
-    }
+    return function dummie() {
+        console.log("I have return");
+    };
 };
 
 function returnsAnAnonymousFunction() {
     return function() {
-        console.log(anonymous);
-    }
+        console.log("I am anonymous");
+    };
 };
+
+
+
+// function receivesAFunction(callback) {
+//     return callback();
+// };
+
+// function returnsANamedFunction() {
+//      return  function named() {
+//         console.log("My return");
+//     }
+// };
+
+// function returnsAnAnonymousFunction() {
+//     return function() {
+//         console.log(anonymous);
+//     }
+// };
